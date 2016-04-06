f08d28e374885354e914554e4c35ada9e3def20f
Tom Astley <tastley@88brandpartners.com>
2016-04-06 09:11:34 -0500

    refactoring. clean up

--- src/js/main.js
+++ src/js/main.js
@@ -48,55 +48,50 @@ console.log("haiiiiii");
 
 // TABBED MODULE MENU CLICK FUNCTIONS
 
-var currentActiveTab='tmi-blue-1-active';
+var currentActiveTab = 'tmi-blue-1-active';
 
 $('.tmi-blue-1').on('click', function() {
-  $('.tabbed-module-content').hide();
- //  	$('.tmi-blue-2').removeClass('tmi-blue-2-active');
-	// $('.tmi-blue-3').removeClass('tmi-blue-3-active');
-	// $('.tmi-blue-4').removeClass('tmi-blue-4-active');
-	$('.tmi-blue-1').addClass("tmi-blue-1-active");
+  	$('.tabbed-module-content').hide();
+		$('.tmi-blue-1').addClass('tmi-blue-1-active');
   	$('.tmc-1').show();
-  	$('.tabbed-menu-item').removeClass($('.' + currentActiveTab))
-  	currentActiveTab=$(this).attr('class').split(' ').pop();
-  	console.log(currentActiveTab);
+
+		if (currentActiveTab !== 'tmi-blue-1-active') {
+				$('.tabbed-menu-item').removeClass(currentActiveTab);
+				currentActiveTab=$(this).attr('class').split(' ').pop();
+		}
+
 });
 
 $('.tmi-blue-2').on('click', function() {
 
 	$('.tabbed-module-content').hide();
+	$('.tmi-blue-2').addClass('tmi-blue-2-active');
 	$('.tmc-2').show();
-	// $('.tmi-blue-1').removeClass('tmi-blue-1-active');
-	// $('.tmi-blue-3').removeClass('tmi-blue-3-active');
-	// $('.tmi-blue-4').removeClass('tmi-blue-4-active');
-	$('.tmi-blue-2').addClass("tmi-blue-2-active");
-	$('.tabbed-menu-item').removeClass($('.' + currentActiveTab))
-  	currentActiveTab=$(this).attr('class').split(' ').pop();
-  	console.log(currentActiveTab);
+
+	if (currentActiveTab !== 'tmi-blue-2-active') {
+			$('.tabbed-menu-item').removeClass(currentActiveTab);
+			currentActiveTab=$(this).attr('class').split(' ').pop();
+	}
 });
 
 $('.tmi-blue-3').on('click', function() {
 	$('.tabbed-module-content').hide();
-	$('.tmc-3').show();
-	// $('.tmi-blue-1').removeClass('tmi-blue-1-active');
-	// $('.tmi-blue-2').removeClass('tmi-blue-2-active');
-	// $('.tmi-blue-4').removeClass('tmi-blue-4-active');
 	$('.tmi-blue-3').addClass("tmi-blue-3-active");
-	$('.tabbed-menu-item').removeClass($('.' + currentActiveTab))
- 	currentActiveTab=$(this).attr('class').split(' ').pop();
-  	console.log(currentActiveTab);
+	$('.tmc-3').show();
+	if (currentActiveTab !== 'tmi-blue-3-active') {
+			$('.tabbed-menu-item').removeClass(currentActiveTab);
+			currentActiveTab=$(this).attr('class').split(' ').pop();
+	}
 });
 
 $('.tmi-blue-4').on('click', function() {
 	$('.tabbed-module-content').hide();
-	$('.tmc-4').show();
-	// $('.tmi-blue-1').removeClass('tmi-blue-1-active');
-	// $('.tmi-blue-3').removeClass('tmi-blue-3-active');
-	// $('.tmi-blue-2').removeClass('tmi-blue-2-active');
 	$('.tmi-blue-4').addClass("tmi-blue-4-active");
-	$('.tabbed-menu-item').removeClass($('.' + currentActiveTab))
-  	currentActiveTab=$(this).attr('class').split(' ').pop();
-  	console.log(currentActiveTab);
+	$('.tmc-4').show();
+	if (currentActiveTab !== 'tmi-blue-4-active') {
+			$('.tabbed-menu-item').removeClass(currentActiveTab);
+			currentActiveTab=$(this).attr('class').split(' ').pop();
+	}
 });
 
 
